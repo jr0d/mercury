b220e241e3c95362c664e3cd12c1056617cd2dfd
Jared Rodriguez <jared.rodriguez@rackspace.com>
2016-02-29 12:29:33 -0600

    transfer

--- src/mercury-agent/mercury/agent/task_runner.py
+++ src/mercury-agent/mercury/agent/task_runner.py
@@ -13,14 +13,27 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import logging
+import time
 import threading
 
 
+log = logging.getLogger(__name__)
+
+
 class TaskRunner(object):
-    def __int__(self, job_id, task_id, entry):
+    def __int__(self, job_id, task_id, entry, entry_args, entry_kwargs):
         self.job_id = job_id
         self.task_id = task_id
         self.entry = entry
+        self.args = entry_args
+        self.kwargs = entry_kwargs
 
         self.time_started, self.time_completed = None
 
+    def __run(self):
+        result = self.entry
+
+
+    def run(self):
+        log.info('Starting task: %s [%s]' % (self.entry.__name__, self.task_id))
