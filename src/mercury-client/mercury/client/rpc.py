9249613a06c6d1aa2b71919721cec74d1b0af873
Jared Rodriguez <jared.rodriguez@rackspace.com>
2016-06-20 17:10:01 -0500

    API and client work

--- src/mercury-client/mercury/client/rpc.py
+++ src/mercury-client/mercury/client/rpc.py
@@ -2,10 +2,23 @@ import json
 import requests
 
 
+def check_error(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except requests.exceptions.HTTPError as http_error:
+            try:
+                data = http_error.response.json()
+            except ValueError:
+                data = http_error.response.text
+            return {'error': True, 'code': http_error.response.status_code, 'data': data}
+    return wrapper
+
+
 class InterfaceBase(object):
     SERVICE_URI = ''
 
-    def __init__(self, target):
+    def __init__(self, target, max_items=100):
         self.target = target
         if self.SERVICE_URI:
             self.base_url = '{0}/{1}'.format(self.target, self.SERVICE_URI)
@@ -13,6 +26,8 @@ class InterfaceBase(object):
             self.base_url = self.target
 
         self.headers = {'Content-type': 'application/json'}
+
+        self.max_items = max_items
 
     def join_endpoint(self, endpoint):
         endpoint = endpoint.strip('/')
@@ -26,17 +41,27 @@ class InterfaceBase(object):
             return self.headers
         return extra_headers.update(self.headers)
 
-    def get(self, endpoint='/', params=None, extra_headers=None):
-        r = requests.get(self.join_endpoint(endpoint), params=params,
+    @check_error
+    def get(self, item='', params=None, extra_headers=None):
+        r = requests.get(self.join_endpoint(item), params=params,
                          headers=self.append_header(extra_headers))
         r.raise_for_status()
         return r.json()
 
-    def post(self, endpoint='/', data=None, params=None, extra_headers=None):
-        r = requests.post(self.join_endpoint(endpoint), params=params, data=data,
+    @check_error
+    def post(self, item='', data=None, params=None, extra_headers=None):
+        r = requests.post(self.join_endpoint(item), params=params, data=json.dumps(data),
                           headers=self.append_header(extra_headers))
         r.raise_for_status()
         return r.json()
+
+    @check_error
+    def query(self, query, item='/query', projection=None, params=None, extra_headers=None):
+        params = params or {}
+        if projection:
+            params['projection'] = ','.join(projection)
+
+        return self.post(item, data={'query': query}, params=params, extra_headers=extra_headers)
 
 
 class JobInterfaceBase(InterfaceBase):
@@ -61,33 +86,28 @@ class JobQuery(JobInterfaceBase):
 
         self.job_id = r['job_id']
 
-    def get_job(self):
-        if not self.job_id:
-            return
 
-        return self.get(self.job_id)
-
-
-class Jobs(JobInterfaceBase):
-    def query(self):
-        pass
+class ActiveComputers(InterfaceBase):
+    SERVICE_URI = 'api/active/computers'
 
 
 if __name__ == '__main__':
-    job_q = JobQuery('http://mercury:9005', {'raid.0.slot': '3'},
-                     {'method': 'hpssa_create_array', 'kwargs': {
-        'slot': 3,
-        'selection': '1I:1:1,1I:1:2,1I:1:3,1I:1:4,1I:1:5,1I:1:6,1I:1:7,1I:1:8',
-        'raid': '1+0'
-    }})
-    job_q.post_job()
+    # job_q = JobQuery('http://mercury:9005', {'raid.0.slot': '3'},
+    #                  {'method': 'hpssa_create_array', 'kwargs': {
+    #     'slot': 3,
+    #     'selection': '1I:1:1,1I:1:2,1I:1:3,1I:1:4,1I:1:5,1I:1:6,1I:1:7,1I:1:8',
+    #     'raid': '1+0'
+    # }})
+    # job_q.post_job()
     from pprint import pprint
-    pprint(job_q.get_job())
-    pprint(job_q.job_id)
-    import time
-    time.sleep(10)
-    pprint(job_q.get_job())
-
-    jobs = Jobs('http://mercury:9005')
-    pprint(jobs.get())
-
+    # pprint(job_q.get_job())
+    # pprint(job_q.job_id)
+    # import time
+    # time.sleep(10)
+    # pprint(job_q.get_job())
+    #
+    # jobs = Jobs('http://mercury:9005')
+    # pprint(jobs.get())
+    ac = ActiveComputers('http://mercury:9005')
+    pprint(ac.get())
+    pprint(ac.query({'mercury_id': '017f3b1adc67a79050e3655747a0b6b0711c81ad68'}, projection=['mercury_id']))
