d711816b50b0f2592b3f98b044c9410e623610db
Jared Rodriguez <jared.rodriguez@rackspace.com>
2016-09-23 10:31:34 -0500

    Change email address

--- src/mercury-agent/mercury/inspector/inspectors/routes.py
+++ src/mercury-agent/mercury/inspector/inspectors/routes.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Jared Rodriguez (jared at blacknode dot net)
+# Copyright 2015 Jared Rodriguez (jared.rodriguez@rackspace.com)
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
