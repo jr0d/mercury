b428791c43cb23cdb69325e2034b3a717d70164b
Jared Rodriguez <jared.rodriguez@rackspace.com>
2017-10-14 00:08:00 -0500

    Goodbye mercury agent

--- src/mercury-agent/mercury/hardware/drivers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .drivers import *
-
-from .hp_raid import *
-from .megaraid import *
-from .obm import *
--- src/mercury-agent/tests/unit/base.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class MercuryAgentUnitTest(unittest.TestCase):
-    """Base class for mercury-agent unit tests."""
-    pass
