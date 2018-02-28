e656642db6f07c4f59faff9d022cf991d085c178
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2018-02-28 15:09:33 -0600

    Initial behave testing code

---
+++ tests/api/features/client.py
@@ -0,0 +1,52 @@
+import copy
+import requests
+
+class APIClient(object):
+
+    def __init__(self, base_url, request_kwargs=None):
+
+        timeout = None
+        ssl_certificate_verify = False
+        verbose = True
+
+        bypass_auth = True
+
+        if request_kwargs:
+            timeout = request_kwargs.get('timeout', timeout)
+            ssl_certificate_verify = request_kwargs.get(
+                'ssl_certificate_verify', ssl_certificate_verify)
+            verbose = request_kwargs.get('verbose', verbose)
+            bypass_auth = request_kwargs.get('bypass_auth', bypass_auth)
+
+        self.base_url = base_url
+        self.headers = {'Content-Type': 'application/json'}
+        if bypass_auth:
+            self.headers['X-Identity-Status'] = 'confirmed'
+        self.verify = ssl_certificate_verify
+        self.verbose = verbose
+
+        self.request_kwargs = dict()
+        self.request_kwargs['url'] = self.base_url
+        self.request_kwargs['headers'] = self.headers
+        self.request_kwargs['verify'] = self.verify
+
+        if timeout:
+            self.request_kwargs['timeout'] = timeout
+
+    def get(self, resource_id=None, params=None, url=None):
+        request_kwargs = copy.deepcopy(self.request_kwargs)
+        if url:
+            request_kwargs['url'] = url
+        if resource_id:
+            resource_url = request_kwargs['url']
+            request_kwargs['url'] = '{0}/{1}'.format(resource_url, resource_id)
+        if params:
+           request_kwargs['params'] = params
+        resp = requests.get(**request_kwargs)
+        if self.verbose:
+            print('{0}GET REQUEST{1}'.format('*' * 20, '*' * 24))
+            print(request_kwargs)
+            print('{0}RESPONSE{1}'.format('*' * 20, '*' * 27))
+            print(resp.content)
+            print('*' * 48)
+        return resp
