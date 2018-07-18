9080e9c967fa5e2af43c42a48de8b8ec3231a866
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2018-07-18 15:25:05 -0500

    Add ip argument to agent running step

--- src/tests/behave/agent/features/steps/common.py
+++ src/tests/behave/agent/features/steps/common.py
@@ -6,9 +6,11 @@ from behave import given, then, when, step, use_step_matcher
 use_step_matcher("parse")
 
 
-@given('something with the agent')
-def step_something_with_the_agent(context):
+@given('an agent is running on {ip}')
+def step_an_agent_is_running_on_ip(context, ip):
     """
     :type context: behave.runner.Context
+    :type ip: str
     """
+
     pass
