d64a8eee57e0bde43864456fc5b30f3a660fa220
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2018-10-26 15:24:16 -0500

    Add new feature to feature file and add support for reading back null responses

--- src/tests/behave/api/features/rpc/capabilities/misc.feature
+++ src/tests/behave/api/features/rpc/capabilities/misc.feature
@@ -27,3 +27,22 @@ Feature: Inject Misc Capability RPC Jobs
         | filename      | out_filename     |
         | run_pwd.json  | run_pwd_out.json |
         | run_ip.json   | run_ip_out.json  |
+
+    # /rpc/jobs run_async command
+    @positive @p0 @smoke
+    @MRC-124
+    @not-local
+    Scenario Outline: Inject rpc run_async capability
+        Given a inventory_computers entity id is located for testing
+        And I have job injection details for a specific inventory_computers device in <filename> for creating jobs using the rpc_jobs api
+        When I get the injection results from a post to rpc_jobs
+        Then the rpc_jobs response status is 200 OK
+        Then the response contains a rpc_jobs job_id
+        And the corresponding rpc_jobs job is completed with successful rpc_tasks tasks
+        And the rpc_jobs response status is 200 OK
+        And the first rpc_tasks task for the inventory_computers device has the output contained in <out_filename>
+
+        Examples: Filenames
+        | filename      | out_filename     |
+        | run_async_pwd.json  | run_async_out.json |
+        | run_async_ip.json   | run_async_out.json  |
--- src/tests/behave/api/features/steps/rpc_capabilities.py
+++ src/tests/behave/api/features/steps/rpc_capabilities.py
@@ -23,48 +23,57 @@ def step_the_first_task_has_output_in_file(
     location = context.json_location
     data = read_json_from_file(out_filename, location)
 
-    stdout = data["stdout"]
-    returncode = data["returncode"]
-    stderr = data["stderr"]
-
-    device_id = context.services[device_service]["id"]
-
-    if "_sub_" in stdout:
-        # We need to get information from the mercury device details
-        # to compare to what was returned from the RPC job
-        prefix, suffix = stdout.split("_sub_")
-        device_resp = device_client.get(device_id)
-
-        # reduce something like active.rpc_port in order ro read it
-        # from a dict, for example resp.json()["active"]["rpc_port"]
-        keys = prefix.split(".")
-        actual_value = reduce(operator.getitem, keys, device_resp.json())
-        stdout = actual_value + suffix
-
     tasks = context.services[service_name]["tasks"]
     first_task = tasks[0]
 
     task_resp = service_client.get(resource_id=first_task["task_id"])
     task_message = task_resp.json()["message"]
 
-    context.check.assertEqual(
-        task_message["stdout"],
-        stdout,
-        msg="Stdout was {}, should be {}".format(
-            task_message["stdout"], stdout
-        ),
-    )
-    context.check.assertEqual(
-        task_message["returncode"],
-        returncode,
-        msg="Returncode was {}, should be {}".format(
-            task_message["returncode"], returncode
-        ),
-    )
-    context.check.assertEqual(
-        task_message["stderr"],
-        stderr,
-        msg="Stderr was {}, should be {}".format(
-            task_message["stderr"], stderr
-        ),
-    )
+    if data["message"] is None:
+        context.check.assertEqual(
+            data["message"],
+            task_message,
+            msg="Message was {}, should be {}".format(
+                task_message, data["message"]
+            ),
+        )
+    else:
+        stdout = data["message"]["stdout"]
+        returncode = data["message"]["returncode"]
+        stderr = data["message"]["stderr"]
+
+        device_id = context.services[device_service]["id"]
+
+        if "_sub_" in stdout:
+            # We need to get information from the mercury device details
+            # to compare to what was returned from the RPC job
+            prefix, suffix = stdout.split("_sub_")
+            device_resp = device_client.get(device_id)
+
+            # reduce something like active.rpc_port in order ro read it
+            # from a dict, for example resp.json()["active"]["rpc_port"]
+            keys = prefix.split(".")
+            actual_value = reduce(operator.getitem, keys, device_resp.json())
+            stdout = actual_value + suffix
+
+        context.check.assertEqual(
+            task_message["stdout"],
+            stdout,
+            msg="Stdout was {}, should be {}".format(
+                task_message["stdout"], stdout
+            ),
+        )
+        context.check.assertEqual(
+            task_message["returncode"],
+            returncode,
+            msg="Returncode was {}, should be {}".format(
+                task_message["returncode"], returncode
+            ),
+        )
+        context.check.assertEqual(
+            task_message["stderr"],
+            stderr,
+            msg="Stderr was {}, should be {}".format(
+                task_message["stderr"], stderr
+            ),
+        )
